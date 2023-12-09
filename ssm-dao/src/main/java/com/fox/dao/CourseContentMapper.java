6cb669a4a457b79e32bde3bb963f3302be32ff10
2899609730 <53003793+2899609730@users.noreply.github.com>
2023-12-09 20:33:51 +0800

    主干修改冲突测试

--- ssm-dao/src/main/java/com/fox/dao/CourseContentMapper.java
+++ ssm-dao/src/main/java/com/fox/dao/CourseContentMapper.java
@@ -10,7 +10,7 @@ public interface CourseContentMapper {
         根据课程id查询关联的章节信息及章节信息关联的课时信息
      */
     public List<CourseSection> findSectionAndLessonByCourseId(Integer courseId);;;
-    // 本地修改5
+    // 主干修改冲突测试
     // 本地修改6
     /*
         回显章节对应的课程信息
