559f1a3cb4f45ee65f1be9cdda9fb9da6b71837b
courage <12>
2023-12-09 20:10:17 +0800

    本地提交

--- ssm-dao/src/main/java/com/fox/dao/CourseContentMapper.java
+++ ssm-dao/src/main/java/com/fox/dao/CourseContentMapper.java
@@ -10,7 +10,8 @@ public interface CourseContentMapper {
         根据课程id查询关联的章节信息及章节信息关联的课时信息
      */
     public List<CourseSection> findSectionAndLessonByCourseId(Integer courseId);;;
-    // 本地修改
+    // 本地修改1
+    // 本地修改2
     /*
         回显章节对应的课程信息
      */
